1aab9acc6771c69447a71ab07a8d3efffdbf96ac
Slim <mslimbeji@gmail.com>
2020-10-09 18:09:20 +0100

    splitting broker/alpaca.py into two files

--- brokers/broker.py
+++ brokers/broker.py
@@ -5,12 +5,3 @@ class Broker:
         self.partially_filled_orders = []
         self.filled_orders = []
         self.canceled_orders = []
-
-    #==== Global helper methods=====================
-
-    @staticmethod
-    def get_chunks(l, chunk_size):
-        chunks = []
-        for i in range(0, len(l), chunk_size):
-            chunks.append(l[i:i + chunk_size])
-        return chunks
--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,2 @@
 from .indicators import *
+from .helpers import *

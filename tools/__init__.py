396105fa95da81be5ea8a063e66063486bc8fb4a
SlimBeji <mslimbeji@gmail.com>
2020-12-02 01:10:32 +0100

    creating decorators that snatch functions locals

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,2 +1,3 @@
+from .decorators import snatch_locals, snatch_method_locals
 from .helpers import *
 from .indicators import *
---
+++ tools/decorators.py
@@ -0,0 +1,74 @@
+import sys
+from copy import deepcopy
+from functools import wraps
+
+
+def call_function_get_frame(func, *args, **kwargs):
+    """
+    Calls the function *func* with the specified arguments and keyword
+    arguments and snatches its local frame before it actually executes.
+    """
+
+    frame = None
+    trace = sys.gettrace()
+
+    def snatch_locals(_frame, name, arg):
+        nonlocal frame
+        if frame is None and name == "call":
+            frame = _frame
+            sys.settrace(trace)
+        return trace
+
+    sys.settrace(snatch_locals)
+    try:
+        result = func(*args, **kwargs)
+    finally:
+        sys.settrace(trace)
+    return frame, result
+
+
+def snatch_locals(store):
+    """Snatch a function local variables
+    and store them in store variable"""
+
+    def wrapper(func_input):
+        @wraps(func_input)
+        def func_output(*args, **kwargs):
+            global store
+            frame, result = call_function_get_frame(func_input, *args, **kwargs)
+            store = frame.f_locals
+            return result
+
+        return func_output
+
+    return wrapper
+
+
+def snatch_method_locals(name, snapshot_before=False, copy_method=deepcopy):
+    """snatch a class method locals and
+    stores in an instance variable"""
+
+    if not isinstance(name, str):
+        raise ValueError(
+            "snatch_method_locals must receive a string as input, received %r instead"
+            % name
+        )
+
+    def wrapper(func_input):
+        @wraps(func_input)
+        def func_output(*args, **kwargs):
+            instance_copy = None
+            if snapshot_before and hasattr(func_input, "__self__"):
+                instance_copy = copy_method(func_input.__self__.__dict__)
+
+            frame, result = call_function_get_frame(func_input, *args, **kwargs)
+            store = frame.f_locals
+            if instance_copy:
+                store["snapshot_before"] = instance_copy
+            instance = store.pop("self")
+            instance.__dict__[name] = store
+            return result
+
+        return func_output
+
+    return wrapper

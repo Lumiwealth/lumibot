f98341a19f68309bf8ce81157fb0bb853860504d
SlimBeji <mslimbeji@gmail.com>
2020-12-02 11:20:03 +0100

    refactoring main.py

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
+import argparse
+import sys
 from datetime import datetime
 
-from backtesting import BacktestingBroker, YahooDataBacktesting
+from backtesting import YahooDataBacktesting
 from brokers import Alpaca
 from credentials import AlpacaConfig
 from data_sources import AlpacaData
 from strategies import Diversification, IntradayMomentum, Momentum, Screener
 from traders import Trader
 
+# Global parameters
+debug = False
+budget = 40000
+backtesting_start = datetime(2010, 6, 1)
+backtesting_end = datetime(2011, 6, 1)
+logfile = "logs/test.log"
+
+# Trading objects
+alpaca_broker = Alpaca(AlpacaConfig)
+alpaca_data_source = AlpacaData(AlpacaConfig)
+trader = Trader(logfile=logfile, debug=debug)
+
+# Strategies mapping
+mapping = {
+    "momentum": Momentum,
+    "diversification": Diversification,
+    "intraday_momentum": IntradayMomentum,
+    "screener": Screener,
+}
+
 if __name__ == "__main__":
-    debug = False
-    budget = 40000
-    trader = Trader(logfile="logs/test.log", debug=debug)
-    broker = Alpaca(AlpacaConfig)
-    pricing_data = AlpacaData(AlpacaConfig)
+    parser = argparse.ArgumentParser("Running AlgoTrader")
+    parser.add_argument("strategies", nargs="+", help="list of strategies")
+    parser.add_argument(
+        "-l",
+        "--live-trading",
+        default=False,
+        action="store_true",
+        help="enable live trading",
+    )
 
-    backtesting_start = datetime(2010, 6, 1)
-    backtesting_end = datetime(2011, 6, 1)
-    yahoo_backtesting_source = YahooDataBacktesting(backtesting_start, backtesting_end)
-    backtesting_broker = BacktestingBroker(yahoo_backtesting_source)
+    args = parser.parse_args()
+    strategies = args.strategies
+    live_trading = args.live_trading
 
-    momentum = Momentum(budget=budget, broker=backtesting_broker)
-    # momentum = Momentum(budget=budget, broker=broker)
-    trader.add_strategy(momentum)
+    for strategy_name in strategies:
+        strategy_class = mapping.get(strategy_name)
+        if strategy_class is None:
+            raise ValueError("Strategy %s does not exist" % strategy_name)
 
-    # diversification = Diversification(budget=budget, broker=backtesting_broker)
-    # diversification = Diversification(budget=budget, broker=broker)
-    # trader.add_strategy(diversification)
+        if live_trading:
+            strategy = strategy_class(budget=budget, broker=alpaca_broker)
+            trader.add_strategy(strategy)
+        else:
+            strategy_class.backtest(YahooDataBacktesting, budget, backtesting_start, backtesting_end)
 
-    # intraday_momentum = IntradayMomentum(budget=budget, broker=backtesting_broker)
-    # intraday_momentum = IntradayMomentum(budget=budget, broker=broker)
-    # trader.add_strategy(intraday_momentum)
-
-    # screener = Screener(budget=budget, broker=backtesting_broker)
-    # screener = Screener(budget=budget, broker=broker)
-    # trader.add_strategy(screener)
-
-    trader.run_all()
+    if live_trading:
+        trader.run_all()
 
     print("The end")

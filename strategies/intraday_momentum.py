7863fa965406688e658fde4132c9df68ec99d6d0
SlimBeji <mslimbeji@gmail.com>
2020-12-29 00:30:03 +0100

    refactoring interday momentum strategy

--- strategies/intraday_momentum.py
+++ strategies/intraday_momentum.py
@@ -10,20 +10,21 @@ class IntradayMomentum(Strategy):
     # =====Overloading lifecycle methods=============
 
     def initialize(self):
-        # setting the momentum period (in minutes) and the counter
+        # setting the momentum period (in minutes) and the sleeptime
         self.momentum_length = 2
-        self.counter = 0
-
-        # there is only one trading operation per day
-        # no need to sleep betwwen iterations
         self.sleeptime = 1
+
         # set the symbols variable and initialize the asset_symbol variable
         self.symbols = ["SPY", "GLD", "TLT", "MSFT", "TSLA"]
         self.asset = ""
         self.quantity = 0
 
     def on_trading_iteration(self):
-        best_asset = self.get_best_asset()
+        momentums = self.get_assets_momentums()
+        momentums.sort(key=lambda x: x.get("return"))
+        best_asset = momentums[-1].get("symbol")
+        logging.info("%s best symbol." % best_asset)
+
         if best_asset != self.asset:
             if self.asset:
                 logging.info("Swapping %s for %s." % (self.asset, best_asset))
@@ -31,14 +32,14 @@ class IntradayMomentum(Strategy):
                 self.submit_order(order)
 
             self.asset = best_asset
-            best_asset_price = self.get_last_price(best_asset)
-            self.quantity = self.unspent_money // best_asset_price
+            best_asset_price = [
+                m["price"] for m in momentums if m["symbol"] == best_asset
+            ][0]
+            self.quantity = self.portfolio_value // best_asset_price
             order = self.create_order(self.asset, self.quantity, "buy")
             self.submit_order(order)
         else:
             logging.info("Keeping %d shares of %s" % (self.quantity, self.asset))
-
-        self.counter += 1
 
     def before_market_closes(self):
         self.sell_all()
@@ -48,7 +49,7 @@ class IntradayMomentum(Strategy):
 
     # =============Helper methods====================
 
-    def get_best_asset(self):
+    def get_assets_momentums(self):
         momentums = []
         for symbol in self.symbols:
             bars_set = self.get_symbol_bars(
@@ -59,9 +60,11 @@ class IntradayMomentum(Strategy):
                 "%s has a return value of %.2f%% over the last %d minutes(s)."
                 % (symbol, 100 * symbol_momentum, self.momentum_length)
             )
-            momentums.append({"symbol": symbol, "return": symbol_momentum})
-
-        momentums.sort(key=lambda x: x.get("return"))
-        best_asset = momentums[-1].get("symbol")
-        logging.info("%s best symbol." % best_asset)
-        return best_asset
+            momentums.append(
+                {
+                    "symbol": symbol,
+                    "price": bars_set.get_last_price(),
+                    "return": symbol_momentum,
+                }
+            )
+        return momentums

347f616c70a4a9b5a0d7dec74cde798dd16a72b3
SlimBeji <mslimbeji@gmail.com>
2021-03-01 11:56:03 +0100

    refactoring trader class

--- lumibot/traders/trader.py
+++ lumibot/traders/trader.py
@@ -7,18 +7,36 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 class Trader:
     def __init__(self, logfile="logs/test.log", debug=False, strategies=None):
         # Setting debug and _logfile parameters and setting global log format
-        self._logfile = logfile
         self.debug = debug
         self.log_format = logging.Formatter("%(asctime)s: %(levelname)s: %(message)s")
+        self.logfile = logfile
 
         # Setting the list of strategies if defined
         self._strategies = strategies if strategies else []
+        self._pool = []
 
     @property
     def is_backtest(self):
         result = False
         if any([s.broker.IS_BACKTESTING_BROKER for s in self._strategies]):
             result = True
+        return result
+
+    def add_strategy(self, strategy):
+        """Adds a strategy to the trader"""
+        self._strategies.append(strategy)
+
+    def run_all(self):
+        """run all strategies"""
+        if self.is_backtest:
+            logging.info("Backtesting starting...")
+
+        signal.signal(signal.SIGINT, self._stop_pool)
+        self._set_logger()
+        self._init_pool()
+        self._start_pool()
+        self._join_pool()
+        result = self._collect_analysis()
         return result
 
     def _set_logger(self):
@@ -37,11 +55,11 @@ class Trader:
             logger.setLevel(logging.INFO)
 
         # Setting file logging
-        if self._logfile:
-            dir = os.path.dirname(os.path.abspath(self._logfile))
+        if self.logfile:
+            dir = os.path.dirname(os.path.abspath(self.logfile))
             if not os.path.exists(dir):
                 os.mkdir(dir)
-            fileHandler = logging.FileHandler(self._logfile, mode="w")
+            fileHandler = logging.FileHandler(self.logfile, mode="w")
             logger.addHandler(fileHandler)
 
         for handler in logger.handlers:
@@ -49,54 +67,30 @@ class Trader:
 
         logger.propagate = True
 
-    def _abrupt_closing(self, sig, frame):
+    def _init_pool(self):
+        self._pool = [strategy._executor for strategy in self._strategies]
+
+    def _start_pool(self):
+        for strategy_thread in self._pool:
+            strategy_thread.start()
+
+    def _join_pool(self):
+        for strategy_thread in self._pool:
+            strategy_thread.join()
+
+    def _stop_pool(self, sig, frame):
         """Run all strategies on_abrupt_closing
         lifecycle method. python signal handlers
         needs two positional arguments, the signal
         and the frame"""
-
         logging.debug("Received signal number %d." % sig)
-        logging.debug("Executing Trader.abrupt_closing in %s frame." % frame)
-
-        for strategy in self._strategies:
-            logging.info(
-                strategy.format_log_message(
-                    "Executing the on_abrupt_closing lifecycle method"
-                )
-            )
-            strategy.on_abrupt_closing()
-            strategy.set_ready_to_close()
-
-        self.wait_for_strategies_to_close()
+        logging.debug("Closing Trader in %s frame." % frame)
+        for strategy_thread in self._pool:
+            strategy_thread.stop()
         logging.info("Trading finished")
-        os._exit(0)
 
-    def add_strategy(self, strategy):
-        """Adds a strategy to the trader"""
-        self._strategies.append(strategy)
-
-    def wait_for_strategies_to_close(self):
-        """Wait for all strategies to finish executing.
-        keeping the instance open until daemon threads
-        finish executing"""
-        while True:
-            if all([s.get_ready_to_close() for s in self._strategies]):
-                break
-
-    def run_all(self):
-        """run all strategies"""
-        if self.is_backtest:
-            logging.info("Backtesting starting...")
-
-        self._set_logger()
-        signal.signal(signal.SIGINT, self._abrupt_closing)
-        with ThreadPoolExecutor(thread_name_prefix="strategy") as executor:
-            tasks = []
-            for strategy in self._strategies:
-                tasks.append(executor.submit(strategy.run))
-
-            results = []
-            for task in as_completed(tasks):
-                results.append(task.result())
-
-        return results
+    def _collect_analysis(self):
+        result = {}
+        for strategy_thread in self._pool:
+            result[strategy_thread.name] = strategy_thread.result
+        return result

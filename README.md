45bb3a11162e2fb27db7550fc4a3160cf6804a11
Neil Murphy <neil@neilmurphy.tech>
2021-05-21 07:47:22 -0400

    Modify asset.py to contain options information. Started README.md documentation.
    Modify asset.py to contain options information. Started README.md documentation.

--- README.md
+++ README.md
@@ -116,6 +116,30 @@ MyStrategy.backtest(
 
 # Entities
 
+## asset
+An asset object represents securities such as stocks or options in Lumibot. Attributes that are 
+tracked for assets are: 
+  - symbol(str): Ticker symbol representing the stock or underlying for options. So for 
+    example if trading IBM calls the symbol would just be `IBM`. 
+  - asset_type(str): Asset type can be either `stock` or `option`. default: `stock`
+  - name(str): Optional to add in the name of the corporation for logging or printout.  
+  #### Options only
+  - expiration (str): Expiration of the options contract. Format is "YYYY-MM-DD".
+  - strike(float): Contract strike price.
+  - right(str): May enter `call` or `put`.
+  - multiplier(float): Contract multiplier to the underlying.
+
+When creating a new security there are two options. 
+1. Security symbol: It is permissable to use the security symbol only when trading 
+   stocks. Lumibot will convert the ticker symbol to an asset behind the scenes.
+   
+2. Asset object: Asset objects may be created at anytime for stocks or options. For 
+   options asset objects are mandatory due to the additional details required to 
+   identify and trade options. 
+   Assets may be created using the `create_asset` method as follows: 
+   
+
+
 ## bars
 
 This object is a wrapper around pandas dataframe and contains bars data. The raw pandas dataframe
--- lumibot/entities/asset.py
+++ lumibot/entities/asset.py
@@ -8,18 +8,36 @@ class Asset:
       - asset_type (string, default: `stock`): One of the following:
         - `stock`
         - `option`
+      If asset_type is `option` then the following fields are mandatory.
+      - expiration (string, "YYYY-MM-DD"): Contract expiration date.
+      - strike (float): Contract strike price.
+      - right(string): `call` or `put`
+      - multiplier (int): Contract leverage over the underlying.
 
 
     """
 
-    def __init__(self, symbol, asset_type="stock", name=""):
-
-        self.asset_types = ['stock', 'option']
+    def __init__(
+        self,
+        symbol,
+        asset_type="stock",
+        name="",
+        expiration=None,
+        strike=None,
+        right=None,
+        multiplier=None,
+    ):
+        self.asset_types = ["stock", "option"]
 
         self.symbol = symbol
         self.asset_type = asset_type
         self.name = name
 
+        # Options
+        self.expiration = expiration
+        self.strike = strike
+        self.right = right
+        self.multiplier = multiplier
 
     @property
     def asset_type(self):
@@ -28,9 +46,27 @@ class Asset:
     @asset_type.setter
     def asset_type(self, value):
         if not value:
-            value = 'stock'
+            value = "stock"
         if value not in self.asset_types:
             raise ValueError(f"Asset asset_type must be one of {self.asset_types}")
         self._asset_type = value
+
+    # Option methods
+    def is_option(self):
+        return self._asset_type == 'option'
+
+    def __repr__(self):
+        stock_repr = f"{self.symbol.upper()}, Type: {self.asset_type} "
+        option_repr = (
+            f"Exp: {self.expiration} "
+            f"Strike: {self.strike} "
+            f"Right: {self.right} "
+        )
+
+        if self.asset_type == 'stock':
+            return stock_repr
+        else:
+            return stock_repr + option_repr
+
 
 

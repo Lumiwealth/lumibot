7a0ac98cd88e1a80807fe7c879061fa5cd809cc2
SlimBeji <mslimbeji@gmail.com>
2021-05-23 10:24:29 +0100

    adding parameters to lumibot.__init__.py

--- lumibot/__init__.py
+++ lumibot/__init__.py
@@ -1,7 +1,14 @@
 import os
-import pickle
+import pytz
 
+# SOURCE PATH
 LUMIBOT_SOURCE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+# GLOBAL PARAMETERS
+LUMIBOT_DEFAULT_TIMEZONE = "America/New_York"
+LUMIBOT_DEFAULT_PYTZ = pytz.timezone(LUMIBOT_DEFAULT_TIMEZONE)
+
+# CACHING CONFIGURATIONS
 LUMIBOT_CACHE_FOLDER = os.path.join(LUMIBOT_SOURCE_PATH, "cache")
 LUMIBOT_DATE_INDEX_FILE = os.path.join(LUMIBOT_CACHE_FOLDER, "date_index.pkl")
 

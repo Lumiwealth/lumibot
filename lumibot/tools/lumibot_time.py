37e37d3763801368d1f1f82618f77b27911460ac
SlimBeji <mslimbeji@gmail.com>
2021-05-31 12:51:11 +0100

    warning only once against using time.sleep

--- lumibot/tools/lumibot_time.py
+++ lumibot/tools/lumibot_time.py
@@ -4,6 +4,7 @@ import time
 from threading import currentThread
 
 old_time_sleep = time.sleep
+warned_against_calling_time_sleep = False
 
 
 def lumibot_sleep(sleeptime):
@@ -11,11 +12,16 @@ def lumibot_sleep(sleeptime):
 
 
 def warning_time_sleep(sleeptime):
+    global warned_against_calling_time_sleep
+
     thread_name = currentThread().getName()
-    authorized_threads_with_sleep = ["^.*_requesting_data_.*$"]
+    authorized_threads_with_sleep = [r"^.*_requesting_data_.*$"]
     if not any([re.match(expr, thread_name) for expr in authorized_threads_with_sleep]):
-        logging.critical(
-            "time.sleep should be used with caution within lumibot, "
-            "especially in backtesting mode."
-        )
+        if warned_against_calling_time_sleep is False:
+            warned_against_calling_time_sleep = True
+            logging.critical(
+                "Time.sleep has been called within thread %s."
+                "time.sleep should be used with caution within lumibot, "
+                "especially in backtesting mode." % thread_name
+            )
     old_time_sleep(sleeptime)

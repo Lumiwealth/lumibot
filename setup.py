b053488dde6a365b696d74c1ef2f19f2dc9f308c
Robert Grzesik <rob.grzesik@gmail.com>
2021-06-10 13:42:53 -0400

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+import json
+
+import requests
+import setuptools
+
+
+def increment_version():
+    resp = requests.get("https://pypi.python.org/pypi/lumibot/json")
+    j = json.loads(resp.content)
+    last_version = j["info"]["version"]
+    version_numbers = last_version.split(".")
+    version_numbers[-1] = str(int(version_numbers[-1]) + 1)
+    new_version = ".".join(version_numbers)
+    return new_version
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lumibot",
+    # version=increment_version(),
+    version="1.0.2",
+    author="Robert Grzesik",
+    author_email="rob@lumiwealth.com",
+    description="Trading Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lumiwealth/lumibot",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "alpaca_trade_api",
+        "yfinance==0.1.59",
+        "pandas",
+        "pandas_datareader",
+        "pandas_market_calendars",
+        "flask-socketio",
+        "flask-sqlalchemy",
+        "flask-marshmallow",
+        "marshmallow-sqlalchemy",
+        "flask-security",
+        "email_validator",
+        "bcrypt",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

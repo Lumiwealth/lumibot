9f1819c391b3c696f7cdddffb54b1695e9337ac6
Robert Grzesik <rob.grzesik@gmail.com>
2021-06-24 16:44:00 -0400

    upgrade version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="lumibot",
     # version=increment_version(),
-    version="1.0.2",
+    version="1.0.3",
     author="Robert Grzesik",
     author_email="rob@lumiwealth.com",
     description="Trading Framework",

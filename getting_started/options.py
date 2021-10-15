5a72b674c684f1c3d63d98fee5f6281ad7ce1033
Neil Murphy <neil@neilmurphy.tech>
2021-10-15 16:28:25 -0400

    Removed print logs and put messages in self.log_message()

--- getting_started/options.py
+++ getting_started/options.py
@@ -18,7 +18,7 @@ underlying 'AAPL' is in `/data`.
 
 
 class Options(Strategy):
-    def initialize(self, assets, sleep_time=1, printon=True):
+    def initialize(self, assets, sleep_time=1):
         # Set the initial variables or constants
         self.assets = assets
         self.underlying_asset = assets[0]
@@ -31,7 +31,6 @@ class Options(Strategy):
         # Our Own Variables
         self.purchase_order = None
         self.traded = False
-        self.printon = printon
 
         # Used to make a single order.
         self.call_option_to_buy = None
@@ -47,23 +46,23 @@ class Options(Strategy):
             return
 
         # Get server timestamp
-        print(self.get_datetime())
+        self.log_message(self.get_datetime())
 
         # Get the last price for `SPY`
         last_price = self.get_last_price(self.underlying_asset)
-        print(f"This is the last price: {last_price}")
+        self.log_message(f"This is the last price: {last_price}")
 
         # Run a basic call for bars on `AAPL`.
         bar = self.get_symbol_bars(self.underlying_asset, self.length)
-        print(bar)
+        self.log_message(bar)
 
         # Get the dataframe from the `bar` and create new columns.
-        print(f"Trading bars for {self.underlying_asset.symbol} are \n{bar}.")
+        self.log_message(f"Trading bars for {self.underlying_asset.symbol} are \n{bar}.")
         if bar is not None:
             df = bar.df
             df["range"] = df["high"] - df["low"]
             df["ma"] = df["close"].rolling(2).mean()
-            print(df[["open", "close", "range", "ma"]])
+            self.log_message(df[["open", "close", "range", "ma"]])
 
         # Get an options quote, data is only available after 19th.
         # So `None` is returned before then.
@@ -76,25 +75,25 @@ class Options(Strategy):
             multiplier=100,
         )
         option_bar = self.get_symbol_bars(self.option, self.length)
-        print(f"Option bars: {option_bar}")
+        self.log_message(f"Option bars: {option_bar}")
 
         # Retrieve the option chains.
         chains = self.get_chains(self.underlying_asset)
         for ex, chain in chains.items():
-            print("Full option chains: \n", ex, chains)
+            self.log_message(f"Full option chains: \n,{ex}, {chains}")
 
         # Get a single option chain for `SMART` in backtesting this will
         # be the same as `chains` as there are not multiple exchanges.
         chain = self.get_chain(chains, exchange="SMART")
-        print("Single option chain: \n", chain)
+        self.log_message(f"Single option chain: \n{chain}")
 
         # Get expiration dates for the chain above.
         expirations = self.get_expiration(chains, exchange="SMART")
-        print("Expirations: ", expirations)
+        self.log_message(f"Expirations:{expirations}")
 
         # Retrieve the multiplier for this chain.
         multiplier = self.get_multiplier(chains)
-        print("Multiplier: ", multiplier)
+        self.log_message("Multiplier:{multiplier}")
 
         # Strikes are retrieved by searching one expiry date and one right.
         # Create an option asset without strikes.
@@ -107,7 +106,7 @@ class Options(Strategy):
             currency="USD",
         )
         strikes = self.get_strikes(strike_asset)
-        print(
+        self.log_message(
             f"These are the strikes for: {strike_asset.symbol} {strike_asset.expiration} "
             f"{strike_asset.right}: {strikes}"
         )
@@ -127,23 +126,21 @@ class Options(Strategy):
             currency="USD",
         )
         option_order = self.create_order(self.call_option_to_buy, 10, "buy")
-        print(f"Order for {option_order} submitted.")
+        self.log_message(f"Order for {option_order} submitted.")
         self.submit_order(option_order)
 
     def on_canceled_order(self, order):
-        if self.printon:
-            print(
-                f"ORDER CANCEL: {self.get_datetime()}, "
-                f"Quantity:     0, price:      0, "
-                f"side: {order.side}"
-            )
+        self.log_message(
+            f"ORDER CANCEL: {self.get_datetime()}, "
+            f"Quantity:     0, price:      0, "
+            f"side: {order.side}"
+        )
 
     def on_filled_order(self, position, order, price, quantity, multiplier):
-        if self.printon:
-            print(
-                f"ORDER FILLED: {self.get_datetime()}, Quantity: {quantity:5.0f}, price:"
-                f" {price:5.2f}, side: {order.side}"
-            )
+        self.log_message(
+            f"ORDER FILLED: {self.get_datetime()}, Quantity: {quantity:5.0f}, price:"
+            f" {price:5.2f}, side: {order.side}"
+        )
 
     def on_abrupt_closing(self):
         self.sell_all()
@@ -236,7 +233,6 @@ if __name__ == "__main__":
 
     kwargs = {
         "assets": list(pandas_data),
-        "printon": True,
         "sleep_time": 10,
     }
     stats_file = f"logs/strategy_{strategy_class.__name__}_{int(time())}.csv"
@@ -250,6 +246,7 @@ if __name__ == "__main__":
         backtesting_end,
         pandas_data=pandas_data,
         stats_file=stats_file,
+        logfile=logfile,
         **kwargs,
     )
     toc = perf_counter()

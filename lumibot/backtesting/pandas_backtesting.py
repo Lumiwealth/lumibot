664d26a41b17d0d00328a182e810723f87b5ef4d
Neil Murphy <neilsmurphy@outlook.com>
2022-01-13 12:34:20 -0500

    Removing copilot code/method.

--- lumibot/backtesting/pandas_backtesting.py
+++ lumibot/backtesting/pandas_backtesting.py
@@ -16,23 +16,3 @@ class PandasDataBacktesting(DataSourceBacktesting, PandasData):
         self.LIVE_DATA_SOURCE = PandasData
         PandasData.__init__(self, pandas_data, **kwargs)
         DataSourceBacktesting.__init__(self, datetime_start, datetime_end)
-
-    def get_data(self, start_date, end_date, **kwargs):
-        """
-        Get the data from the data source.
-
-        Parameters
-        ----------
-        start_date : datetime.datetime
-            The start date of the data to get.
-        end_date : datetime.datetime
-            The end date of the data to get.
-        kwargs : dict
-            Additional arguments to pass to the data source.
-
-        Returns
-        -------
-        pandas.DataFrame
-            The data.
-        """
-        return self.data_source.get_data(start_date, end_date, **kwargs)

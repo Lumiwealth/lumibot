731449d83b08114b6b750e27c13e9497674e38d9
Robert Grzesik <rob.grzesik@gmail.com>
2022-06-20 21:04:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lumibot",
-    version="2.1.9",
+    version="2.2.0",
     author="Robert Grzesik",
     author_email="rob@lumiwealth.com",
     description="Trading Framework",

671cb91ba7152bf95ca313a920f68ec2feefa6f1
Robert Grzesik <rob.grzesik@gmail.com>
2022-12-08 17:10:01 -0500

    bug fix for getting IB futures prices

--- lumibot/brokers/interactive_brokers.py
+++ lumibot/brokers/interactive_brokers.py
@@ -39,7 +39,7 @@ class InteractiveBrokers(InteractiveBrokersData, Broker):
 
         # Connection to interactive brokers
         self.ib = None
-        
+
         # check if the config is a dict
         if isinstance(config, dict):
             ip = config["IP"]
@@ -49,9 +49,9 @@ class InteractiveBrokers(InteractiveBrokersData, Broker):
             ip = config.IP
             socket_port = config.SOCKET_PORT
             client_id = config.CLIENT_ID
-            
+
         self.start_ib(ip, socket_port, client_id)
-        
+
         self.market = None
 
     def start_ib(self, ip, socket_port, client_id):
@@ -1311,9 +1311,8 @@ class IBApp(IBWrapper, IBClient):
             contract.multiplier = asset.multiplier
             contract.primaryExchange = "CBOE"
         elif asset.asset_type == "future":
-            contract.lastTradeDateOrContractMonth = asset.expiration.strftime("%Y%m")
-            contract.primaryExchange = "GLOBEX"
-            contract.currency = "USD"
+            contract.includeExpired = True
+            contract.lastTradeDateOrContractMonth = asset.expiration.strftime("%Y%m%d")
         elif asset.asset_type == "forex":
             contract.exchange = "IDEALPRO"
         elif asset.asset_type == "index":

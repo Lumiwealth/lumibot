d9d20e58d4fd219aaf73dd26f3cf9e9d4e31a287
TrainedPro <hassaananwar12345@gmail.com>
2023-08-14 21:43:58 +0500

    Updated alpaca_data.py To Work The Same As Other Brokers
    Alpaca would previously return fewer number of rows than requested even when it was required. This was a bug recently introduced, which could cause the backtesting to work perfectly but not live as alpaca would not return an appropriate amount of rows in the dataframe. Thus any calculations being done with Alpaca's historical data may not work

--- lumibot/data_sources/alpaca_data.py
+++ lumibot/data_sources/alpaca_data.py
@@ -14,245 +14,231 @@ from .data_source import DataSource
 
 
 class AlpacaData(DataSource):
-    SOURCE = "ALPACA"
-    MIN_TIMESTEP = "minute"
-    TIMESTEP_MAPPING = [
-        {
-            "timestep": "minute",
-            "representations": [TimeFrame(1, TimeFrameUnit.Minute), "minute"],
-        },
-        {
-            "timestep": "day",
-            "representations": [TimeFrame(1, TimeFrameUnit.Day), "day"],
-        },
-    ]
+	SOURCE = "ALPACA"
+	MIN_TIMESTEP = "minute"
+	TIMESTEP_MAPPING = [
+		{
+			"timestep": "minute",
+			"representations": [TimeFrame(1, TimeFrameUnit.Minute), "minute"],
+		},
+		{
+			"timestep": "day",
+			"representations": [TimeFrame(1, TimeFrameUnit.Day), "day"],
+		},
+	]
 
-    """Common base class for data_sources/alpaca and brokers/alpaca"""
+	"""Common base class for data_sources/alpaca and brokers/alpaca"""
 
-    @staticmethod
-    def _format_datetime(dt):
-        return pd.Timestamp(dt).isoformat()
+	@staticmethod
+	def _format_datetime(dt):
+		return pd.Timestamp(dt).isoformat()
 
-    def __init__(self, config, max_workers=20, chunk_size=100, **kwargs):
-        # Alpaca authorize 200 requests per minute and per API key
-        # Setting the max_workers for multithreading with a maximum
-        # of 200
-        self.name = "alpaca"
-        self.max_workers = min(max_workers, 200)
+	def __init__(self, config, max_workers=20, chunk_size=100, **kwargs):
+		# Alpaca authorize 200 requests per minute and per API key
+		# Setting the max_workers for multithreading with a maximum
+		# of 200
+		self.name = "alpaca"
+		self.max_workers = min(max_workers, 200)
 
-        # When requesting data for assets for example,
-        # if there is too many assets, the best thing to do would
-        # be to split it into chunks and request data for each chunk
-        self.chunk_size = min(chunk_size, 100)
+		# When requesting data for assets for example,
+		# if there is too many assets, the best thing to do would
+		# be to split it into chunks and request data for each chunk
+		self.chunk_size = min(chunk_size, 100)
 
-        # Connection to alpaca REST API
-        self.config = config
+		# Connection to alpaca REST API
+		self.config = config
 
-        if type(config) == dict and "API_KEY" in config:
-            self.api_key = config["API_KEY"]
-        elif hasattr(config, "API_KEY"):
-            self.api_key = config.API_KEY
-        else:
-            raise ValueError("API_KEY not found in config")
+		if type(config) == dict and "API_KEY" in config:
+			self.api_key = config["API_KEY"]
+		elif hasattr(config, "API_KEY"):
+			self.api_key = config.API_KEY
+		else:
+			raise ValueError("API_KEY not found in config")
 
-        if type(config) == dict and "API_SECRET" in config:
-            self.api_secret = config["API_SECRET"]
-        elif hasattr(config, "API_SECRET"):
-            self.api_secret = config.API_SECRET
-        else:
-            raise ValueError("API_SECRET not found in config")
+		if type(config) == dict and "API_SECRET" in config:
+			self.api_secret = config["API_SECRET"]
+		elif hasattr(config, "API_SECRET"):
+			self.api_secret = config.API_SECRET
+		else:
+			raise ValueError("API_SECRET not found in config")
 
-        if type(config) == dict and "ENDPOINT" in config:
-            self.endpoint = config["ENDPOINT"]
-        elif hasattr(config, "ENDPOINT"):
-            self.endpoint = URL(config.ENDPOINT)
-        else:
-            self.endpoint = URL("https://paper-api.alpaca.markets")
+		if type(config) == dict and "ENDPOINT" in config:
+			self.endpoint = config["ENDPOINT"]
+		elif hasattr(config, "ENDPOINT"):
+			self.endpoint = URL(config.ENDPOINT)
+		else:
+			self.endpoint = URL("https://paper-api.alpaca.markets")
 
-        if type(config) == dict and "VERSION" in config:
-            self.version = config["VERSION"]
-        elif hasattr(config, "VERSION"):
-            self.version = config.VERSION
-        else:
-            self.version = "v2"
+		if type(config) == dict and "VERSION" in config:
+			self.version = config["VERSION"]
+		elif hasattr(config, "VERSION"):
+			self.version = config.VERSION
+		else:
+			self.version = "v2"
 
-        self.api = tradeapi.REST(
-            self.api_key, self.api_secret, self.endpoint, self.version
-        )
+		self.api = tradeapi.REST(
+			self.api_key, self.api_secret, self.endpoint, self.version
+		)
 
-    def get_last_price(self, asset, quote=None, exchange=None, **kwargs):
-        if quote is not None:
-            # If the quote is not None, we use it even if the asset is a tuple
-            if type(asset) == Asset and asset.asset_type == "stock":
-                symbol = asset.symbol
-            elif isinstance(asset, tuple):
-                symbol = f"{asset[0].symbol}{quote.symbol}"
-            else:
-                symbol = f"{asset.symbol}{quote.symbol}"
-        elif isinstance(asset, tuple):
-            symbol = f"{asset[0].symbol}{asset[1].symbol}"
-        else:
-            symbol = asset.symbol
+	def get_last_price(self, asset, quote=None, exchange=None, **kwargs):
+		if quote is not None:
+			# If the quote is not None, we use it even if the asset is a tuple
+			if type(asset) == Asset and asset.asset_type == "stock":
+				symbol = asset.symbol
+			elif isinstance(asset, tuple):
+				symbol = f"{asset[0].symbol}{quote.symbol}"
+			else:
+				symbol = f"{asset.symbol}{quote.symbol}"
+		elif isinstance(asset, tuple):
+			symbol = f"{asset[0].symbol}{asset[1].symbol}"
+		else:
+			symbol = asset.symbol
 
-        if isinstance(asset, tuple) and asset[0].asset_type == "crypto":
-            try:
-                trade = self.api.get_latest_crypto_trade(symbol, exchange="CBSE")
-            except:
-                # Fallback exchange if the crypto trade is not found
-                trade = self.api.get_latest_crypto_trade(symbol, exchange="FTXU")
-        elif isinstance(asset, Asset) and asset.asset_type == "crypto":
-            try:
-                trade = self.api.get_latest_crypto_trade(symbol, exchange="CBSE")
-            except:
-                # Fallback exchange if the crypto trade is not found
-                trade = self.api.get_latest_crypto_trade(symbol, exchange="FTXU")
-        else:
-            trade = self.api.get_latest_trade(symbol)
+		if isinstance(asset, tuple) and asset[0].asset_type == "crypto":
+			try:
+				trade = self.api.get_latest_crypto_trade(symbol, exchange="CBSE")
+			except:
+				# Fallback exchange if the crypto trade is not found
+				trade = self.api.get_latest_crypto_trade(symbol, exchange="FTXU")
+		elif isinstance(asset, Asset) and asset.asset_type == "crypto":
+			try:
+				trade = self.api.get_latest_crypto_trade(symbol, exchange="CBSE")
+			except:
+				# Fallback exchange if the crypto trade is not found
+				trade = self.api.get_latest_crypto_trade(symbol, exchange="FTXU")
+		else:
+			trade = self.api.get_latest_trade(symbol)
 
-        return trade.p
+		return trade.p
 
-    def get_barset_from_api(
-        self, api, asset, freq, limit=None, end=None, start=None, quote=None
-    ):
-        """
-        gets historical bar data for the given stock symbol
-        and time params.
+	def get_barset_from_api(
+		self, api, asset, freq, limit=None, end=None, start=None, quote=None
+	):
+		"""
+		gets historical bar data for the given stock symbol
+		and time params.
 
-        outputs a dataframe open, high, low, close columns and
-        a UTC timezone aware index.
-        """
-        if isinstance(asset, tuple):
-            if quote is None:
-                quote = asset[1]
-            asset = asset[0]
+		outputs a dataframe open, high, low, close columns and
+		a UTC timezone aware index.
+		"""
+		if isinstance(asset, tuple):
+			if quote is None:
+				quote = asset[1]
+			asset = asset[0]
 
-        if limit is None:
-            limit = 1000
+		if limit is None:
+			limit = 1000
 
-        if end is None:
-            end = datetime.now(timezone.utc)
+		if end is None:
+			end = datetime.now(timezone.utc) - timedelta(minutes=15) # alpaca limitation of not getting the most recent 15 minutes
 
-        df_ret = None
+		if start is None:
+			if str(freq) == "1Min":
+				if datetime.now().weekday() == 0: # for Mondays as prior days were off
+					loop_limit = limit + 4896 # subtract 4896 minutes to take it from Monday to Friday, as there is no data between Friday 4:00 pm and Monday 9:30 pm causing an incomplete or empty dataframe
+				else:
+					loop_limit = limit
+		
+			elif str(freq) == "1Day":
+				loop_limit = limit * 1.5 # number almost perfect for normal weeks where only weekends are off
 
-        curr_end = end.isoformat() if not isinstance(end, str) else end
+		end = end.isoformat(timespec="seconds")
+		df = [] # to use len(df) below without an error
 
-        if start is None:
-            if str(freq) == "1Min":
-                start = end - timedelta(minutes=limit)
-            elif str(freq) == "1Day":
-                start = end - timedelta(days=limit)
+		while loop_limit / limit <= 64 and len(df) < limit: # arbitrary limit of upto 4 calls after which it will give up
+			if str(freq) == "1Min":
+				start = datetime.fromisoformat(end) - timedelta(minutes=loop_limit)
+				start = start.isoformat(timespec="seconds")
 
-        curr_start = start.isoformat(timespec="seconds")
+			elif str(freq) == "1Day":
+				start = datetime.fromisoformat(end) - timedelta(days=loop_limit)
+				start = start.isoformat(timespec="seconds")
 
-        cnt = 0
-        last_curr_end = None
-        loop_limit = 1000 if limit > 1000 else limit
-        while True:
-            cnt += 1
-            # freqnum = re.search(r'\d+', freq).group()
-            # freqtimelen = freq[len(freqnum):]
-            if asset.asset_type == "crypto":
-                symbol = f"{asset.symbol}{quote.symbol}"
-                barset = api.get_crypto_bars(
-                    symbol,
-                    freq,
-                    end=curr_end,
-                    start=curr_start,  # limit=loop_limit,
-                )
-            else:
-                symbol = asset.symbol
-                barset = api.get_bars(
-                    symbol, freq, limit=loop_limit, end=curr_end, start=curr_start
-                )
-            df = barset.df
+			if asset.asset_type == "crypto":
+				symbol = f"{asset.symbol}{quote.symbol}"
+				barset = api.get_crypto_bars(
+					symbol,
+					freq,
+					start=start, 
+					end=end
+				)
 
-            if df.empty:
-                logging.error(
-                    f"Could not get any pricing data from Alpaca for {symbol}, the DataFrame came back empty"
-                )
-                return None
+			else:
+				symbol = asset.symbol
+				barset = api.get_bars(
+					symbol,
+					freq,
+					start=start,
+					end=end
+				)
+			df = barset.df
 
-            if df_ret is None:
-                df_ret = df
-            elif str(df.index[0]) < str(df_ret.index[0]):
-                df_ret = df.append(df_ret)
+			if df.empty:
+				logging.error(
+					f"Could not get any pricing data from Alpaca for {symbol}, the DataFrame came back empty"
+				)
+				return None
 
-            if len(df_ret) >= limit:
-                break
-            else:
-                curr_end = (
-                    datetime.fromisoformat(str(df_ret.index[0])).strftime(
-                        "%Y-%m-%dT%H:%M:%S"
-                    )
-                    + "-04:00"
-                )
+			df = df[~df.index.duplicated(keep="first")]
+			df = df.iloc[-limit:]
+			df = df[df.close > 0]
+			loop_limit *= 2
+		
+		if len(df) < limit:
+			logging.warning(
+				f"Dataframe for {symbol} has {len(df)} rows while {limit} were requested. Further data does not exist for Alpaca"
+			)
 
-            # Sometimes the beginning date we put in is not a trading date,
-            # this makes sure that we end when we're close enough
-            # (it's just returning the same thing over and over)
-            if curr_end == last_curr_end:
-                break
-            else:
-                last_curr_end = curr_end
+		return df
 
-            # Sleep so that we don't trigger rate limiting
-            if cnt >= 50:
-                time.sleep(10)
-                cnt = 0
+	def _pull_source_bars(
+		self, assets, length, timestep=MIN_TIMESTEP, timeshift=None, quote=None,  include_after_hours=True
+	):
+		"""pull broker bars for a list assets"""
+		if timeshift is None and timestep == "day":
+			# Alpaca throws an error if we don't do this and don't have a data subscription because
+			# they require a subscription for historical data less than 15 minutes old
+			timeshift = timedelta(minutes=16)
 
-        df_ret = df_ret[~df_ret.index.duplicated(keep="first")]
-        df_ret = df_ret.iloc[-limit:]
+		parsed_timestep = self._parse_source_timestep(timestep, reverse=True)
+		kwargs = dict(limit=length)
+		if timeshift:
+			end = datetime.now() - timeshift
+			end = self.to_default_timezone(end)
+			kwargs["end"] = end
 
-        return df_ret[df_ret.close > 0]
+		result = {}
+		for asset in assets:
+			data = self.get_barset_from_api(
+				self.api, asset, parsed_timestep, quote=quote, **kwargs
+			)
+			result[asset] = data
 
-    def _pull_source_bars(
-        self, assets, length, timestep=MIN_TIMESTEP, timeshift=None, quote=None,  include_after_hours=True
-    ):
-        """pull broker bars for a list assets"""
-        if timeshift is None and timestep == "day":
-            # Alpaca throws an error if we don't do this and don't have a data subscription because
-            # they require a subscription for historical data less than 15 minutes old
-            timeshift = timedelta(minutes=16)
+		return result
 
-        parsed_timestep = self._parse_source_timestep(timestep, reverse=True)
-        kwargs = dict(limit=length)
-        if timeshift:
-            end = datetime.now() - timeshift
-            end = self.to_default_timezone(end)
-            kwargs["end"] = end
+	def _pull_source_symbol_bars(
+		self,
+		asset,
+		length,
+		timestep=MIN_TIMESTEP,
+		timeshift=None,
+		quote=None,
+		exchange=None,
+		include_after_hours=True
+	):
+		if exchange is not None:
+			logging.warning(
+				f"the exchange parameter is not implemented for AlpacaData, but {exchange} was passed as the exchange"
+			)
 
-        result = {}
-        for asset in assets:
-            data = self.get_barset_from_api(
-                self.api, asset, parsed_timestep, quote=quote, **kwargs
-            )
-            result[asset] = data
+		"""pull broker bars for a given asset"""
+		response = self._pull_source_bars(
+			[asset], length, timestep=timestep, timeshift=timeshift, quote=quote
+		)
+		return response[asset]
 
-        return result
-
-    def _pull_source_symbol_bars(
-        self,
-        asset,
-        length,
-        timestep=MIN_TIMESTEP,
-        timeshift=None,
-        quote=None,
-        exchange=None,
-        include_after_hours=True
-    ):
-        if exchange is not None:
-            logging.warning(
-                f"the exchange parameter is not implemented for AlpacaData, but {exchange} was passed as the exchange"
-            )
-
-        """pull broker bars for a given asset"""
-        response = self._pull_source_bars(
-            [asset], length, timestep=timestep, timeshift=timeshift, quote=quote
-        )
-        return response[asset]
-
-    def _parse_source_symbol_bars(self, response, asset, quote=None, length=None):
-        # TODO: Alpaca return should also include dividend yield
-        response["return"] = response["close"].pct_change()
-        bars = Bars(response, self.SOURCE, asset, raw=response, quote=quote)
-        return bars
+	def _parse_source_symbol_bars(self, response, asset, quote=None, length=None):
+		# TODO: Alpaca return should also include dividend yield
+		response["return"] = response["close"].pct_change()
+		bars = Bars(response, self.SOURCE, asset, raw=response, quote=quote)
+		return bars

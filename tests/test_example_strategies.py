71162c8b1967c1aa2d8a434d05f96574fd20b511
davidlatte <david.macleod@lattesolution.com>
2023-08-25 17:18:09 -0700

    Eliminating even more rounding error.

--- tests/test_example_strategies.py
+++ tests/test_example_strategies.py
@@ -68,7 +68,7 @@ class TestExampleStrategies:
         # Check that the results are correct
         assert round(results["cagr"] * 100, 1) == 289.7
         assert round(results["volatility"] * 100, 0) == 41.0
-        assert round(results["sharpe"], 3) == 6.932
+        assert round(results["sharpe"], 2) == 6.93
         assert round(results["total_return"] * 100, 1) == 0.7
         assert round(results["max_drawdown"]["drawdown"] * 100, 1) == 0.0
 
@@ -99,7 +99,6 @@ class TestExampleStrategies:
         # Check that the results are correct
         assert round(results["cagr"] * 100, 1) == -69.5
         assert round(results["volatility"] * 100, 1) == 22.1
-        assert round(results["sharpe"], 3) == -3.382
+        assert round(results["sharpe"], 2) == -3.38
         assert round(results["total_return"] * 100, 1) == -1.9
         assert round(results["max_drawdown"]["drawdown"] * 100, 1) == 2.1
-        

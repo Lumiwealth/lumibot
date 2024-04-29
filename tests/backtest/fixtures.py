74831f6943dd80446748ebca883cccb6fad237c3
Ruben Cancho <304053+Canx@users.noreply.github.com>
2024-04-29 11:05:19 +0200

    No spanish ;)

--- tests/backtest/fixtures.py
+++ tests/backtest/fixtures.py
@@ -1,16 +1,14 @@
 import pytest
 import datetime
-from lumibot.backtesting import PolygonDataBacktesting  # Asegúrate de importar correctamente tu clase
+from lumibot.backtesting import PolygonDataBacktesting
 
 @pytest.fixture
 def polygon_data_backtesting():
-    # Configura aquí las propiedades iniciales necesarias para tu objeto de prueba
     datetime_start = datetime.datetime(2023, 1, 1)
     datetime_end = datetime.datetime(2023, 2, 1)
     api_key = "fake_api_key"
     pandas_data = {}
     
-    # Crea una instancia de PolygonDataBacktesting con los valores necesarios
     polygon_data_instance = PolygonDataBacktesting(
         datetime_start=datetime_start,
         datetime_end=datetime_end,

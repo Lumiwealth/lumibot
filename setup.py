5351e6b6fffaceb140d4ef7fd677fc28812a20e1
Robert Grzesik <rob.grzesik@gmail.com>
2024-05-10 14:39:56 -0400

    added ability to backtest index options

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lumibot",
-    version="3.4.0",
+    version="3.4.1",
     author="Robert Grzesik",
     author_email="rob@lumiwealth.com",
     description="Backtesting and Trading Library, Made by Lumiwealth",

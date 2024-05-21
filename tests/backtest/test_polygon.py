16165ee4917d84f665fd55b767c02ec92829bb2f
Ruben Cancho <304053+Canx@users.noreply.github.com>
2024-05-21 17:09:47 +0200

    this is the correct volume

--- tests/backtest/test_polygon.py
+++ tests/backtest/test_polygon.py
@@ -380,7 +380,7 @@ class TestPolygonDataSource:
                 "high": 494.3200,
                 "low": 492.03,
                 "close": 493.82,
-                "volume": 54891827.0
+                "volume": 54775803.0
             },
         ], index="datetime")
         expected_df.index = pd.to_datetime(expected_df.index).tz_convert(tzinfo)

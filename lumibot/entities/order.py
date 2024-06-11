2708ab9c3a7e9bf34e63f20cf6ff6559edf20926
Robert Grzesik <rob.grzesik@gmail.com>
2024-06-11 15:17:33 -0400

    bug fix for order quantity of zero

--- lumibot/entities/order.py
+++ lumibot/entities/order.py
@@ -250,10 +250,6 @@ class Order:
         # Check that quantity is a number
         if not isinstance(quantity, (int, float, Decimal)):
             raise ValueError("Order quantity must be a number")
-        
-        # Check that quantity is not zero
-        if quantity == 0:
-            raise ValueError("Order quantity cannot be zero")
 
         # It is possible for crypto currencies to arrive as a tuple of
         # two assets.

849ea149bdc5b25a8017512e1c0f35e421630723
Haochi Li <haochicheers@gmail.com>
2024-08-06 18:11:46 -0700

    Removed print of in 'BacktestingBroker, asset'

--- lumibot/backtesting/backtesting_broker.py
+++ lumibot/backtesting/backtesting_broker.py
@@ -637,8 +637,6 @@ class BacktestingBroker(Broker):
                 low = df["low"].iloc[0]
                 close = df["close"].iloc[0]
                 volume = df["volume"].iloc[0]
-                print(f"\nin BacktestingBroker, asset: {asset}, dt: {dt}")
-                print(f"in BacktestingBroker, OHLCV: {open}, {high}, {low}, {close}, {volume}")
 
             #############################
             # Determine transaction price.

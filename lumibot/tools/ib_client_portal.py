25de47083260ef607b9699655b7a76f079c2e0e7
Martin Pelteshki <39273158+Al4ise@users.noreply.github.com>
2024-09-03 16:02:34 +0300

    all broken

--- lumibot/tools/ib_client_portal.py
+++ lumibot/tools/ib_client_portal.py
@@ -4,37 +4,36 @@ import time
 import requests
 import os
 import urllib3
+import requests
 
 urllib3.disable_warnings()
 
 class IBClientPortal:
     def __init__(self, config, api_url):
         if api_url is None:
-            self.port = "3000"
-            self.base_url = f'https://localhost:{self.port}'
+            self.port = "4234"
+            self.base_url = f'https://localhost:{self.port}/v1/api'
         else:
             self.api_url = api_url
-            self.base_url = api_url
+            self.base_url = f'{api_url}/v1/api'
 
-        self.account_info_endpoint = f'{self.base_url}/v1/portal/account/summary'
+        self.account_info_endpoint = f'{self.base_url}/portal/account/summary'
         
         self.ib_username = config["IB_USERNAME"]
         self.ib_password = config["IB_PASSWORD"]
 
-        self.last_tickled = time.time()
-
         self.start_ibeam()
-
+        
         # Set self.account_id
         if config["ACCOUNT_ID"]:
             self.account_id = config["ACCOUNT_ID"]
         else:
-            url = f'{self.base_url}/v1/api/portfolio/accounts'
-            response = self.get_json_from_endpoint(url, "Fetching Account ID")
+            url = f'{self.base_url}/portfolio/accounts'
+            response = self.get_from_endpoint(url, "Fetching Account ID")
             if response is not None:
                 self.account_id = response[0]['id']
             else:
-                logging.error(f"Failed to get account id.")
+                logging.error(f"Failed to get Account ID.")
         
     def start_ibeam(self):
         if not hasattr(self, "api_url"):
@@ -51,22 +50,36 @@ class IBClientPortal:
                 'IBEAM_PASSWORD': self.ib_password,
                 'IBEAM_GATEWAY_BASE_URL': self.base_url,
                 'IBEAM_LOG_TO_FILE': False,
-                'IBEAM_REQUEST_RETRIES': 3,
+                'IBEAM_REQUEST_RETRIES': 10,
+                'IBEAM_PAGE_LOAD_TIMEOUT': 30,
                 'IBEAM_INPUTS_DIR': inputs_dir
             }
+            
             env_args = [f'--env={key}={value}' for key, value in env_variables.items()]
-            conf_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "ib-rest-conf.yaml"))
+            
+            conf_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "conf.yaml"))
             volume_mount = f'{conf_path}:{inputs_dir}'
 
             subprocess.run(['docker', 'run', '-d', *env_args, '-p', f'{self.port}:{self.port}', '-v', volume_mount, 'voyz/ibeam'], stdout=subprocess.DEVNULL, text=True)
 
-        while not self.is_client_portal_running():
+        # check if authenticated
+        time.sleep(10)
+
+        while not self.is_authenticated():
             time.sleep(10)
-            
+        
         logging.info("Started IBeam")
 
+    def is_authenticated(self):
+        url = f'{self.base_url}/portfolio/accounts'
+        response = self.get_from_endpoint(url, "Auth Check")
+        if response is not None:
+            return True
+        else:
+            return False
+        
     def getConID(self, symbol, asset_type):
-        url = f"{self.base_url}/v1/api/iserver/secdef/search"
+        url = f"{self.base_url}/iserver/secdef/search"
 
         body = {
             "symbol": symbol,
@@ -82,25 +95,22 @@ class IBClientPortal:
             return None
     
     def is_client_portal_running(self):
-        url = f'{self.base_url}/v1/api/iserver/auth/status'
-        response = self.get_json_from_endpoint(url, "Status Check", silent=True)
-        try:
-            if response["authenticated"] is True:
-                return True
-            else:
-                return False
-        except:
+        url = f'{self.base_url}/iserver/accounts'
+        response = self.get_from_endpoint(url, "Getting Accounts")
+        if response:
+            return True
+        else:
             return False
 
     def get_contract_details_for_contract(self, contract):
         conid=contract.conId
-        url = f"{self.base_url}/v1/api/iserver/contract/{conid}/info"
-        response = self.get_json_from_endpoint(url, "Getting contract details")
+        url = f"{self.base_url}/iserver/contract/{conid}/info"
+        response = self.get_from_endpoint(url, "Getting contract details")
         return response
     
     def get_account_info(self):
         url = f"{self.account_info_endpoint}?accountId={self.account_id}"
-        response = self.get_json_from_endpoint(url, "Getting account info")
+        response = self.get_from_endpoint(url, "Getting account info")
         return response
         
     def get_account_balances(self):
@@ -108,24 +118,20 @@ class IBClientPortal:
         Retrieves the account balances for a given account ID.
         """
         # Define the endpoint URL for fetching account balances
-        url = f"{self.base_url}/v1/portal/portfolio/{self.account_id}/ledger"
-        response = self.get_json_from_endpoint(url, "Getting account balances")
+        url = f"{self.base_url}/portal/portfolio/{self.account_id}/ledger"
+        response = self.get_from_endpoint(url, "Getting account balances")
         return response
         
-    def tryTickle(self):
-        if time.time() - self.last_tickled <= 50:
-            return
-        
+    def tickle(self):        
         try:
-            url = f'{self.base_url}/v1/api/tickle'
-            r = self.get_json_from_endpoint(url, "Tickle", silent=True)
-        except:
-            pass
-        
-        self.last_tickled = time.time()
-    
-    def get_json_from_endpoint(self, endpoint, description, silent=False):
-        self.tryTickle()
+            url = f'{self.base_url}/tickle'
+            self.get_from_endpoint(url, "Tickle", silent=False)
+            return True
+        except Exception as e:
+            logging.error(f"Failed to tickle: {e}")
+            return False
+            
+    def get_from_endpoint(self, endpoint, description, silent=False):
         try:
             # Make the request to the endpoint
             response = requests.get(endpoint, verify=False)
@@ -138,6 +144,10 @@ class IBClientPortal:
                 if not silent:
                     logging.warning(f"{description} endpoint not found.")
                 return None
+            elif response.status_code == 429:
+                logging.info(f"You got rate limited {description}. Waiting for 5 seconds...")
+                time.sleep(5)
+                return self.get_from_endpoint(endpoint, description, silent)
             else:
                 # Log an error message if the request failed
                 if not silent:
@@ -151,7 +161,6 @@ class IBClientPortal:
             return None
     
     def post_to_endpoint(self, url, json):
-        self.tryTickle()
         try:
             response = requests.post(url, json=json, verify=False)
             response.raise_for_status()
@@ -162,12 +171,12 @@ class IBClientPortal:
     
     def get_open_orders(self):
         # Define the endpoint URL for fetching account balances
-        url = f'{self.base_url}/v1/api/iserver/account/orders'
-        response = self.get_json_from_endpoint(url, "Getting open orders")
+        url = f'{self.base_url}/iserver/account/orders'
+        response = self.get_from_endpoint(url, "Getting open orders")
         return response
     
     def execute_order(self, order_data):
-        url = f'{self.base_url}/v1/iserver/account/{self.account_id}/orders'
+        url = f'{self.base_url}/iserver/account/{self.account_id}/orders'
         response = self.post_to_endpoint(url, order_data)
         return response["id"]
             
@@ -175,8 +184,8 @@ class IBClientPortal:
         """
         Retrieves the current positions for a given account ID.
         """
-        url = f"{self.base_url}/v1/portal/portfolio/{self.account_id}/positions"
-        response = self.get_json_from_endpoint(url, "Getting account positions")
+        url = f"{self.base_url}/portal/portfolio/{self.account_id}/positions"
+        response = self.get_from_endpoint(url, "Getting account positions")
         return response
 
     def stop(self):        

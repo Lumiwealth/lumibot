792c3a469bdfa989bd84515d728240ca6d84b335
Ima Vafaei <ima.vafaei@aipoint.io>
2024-09-27 13:09:57 +1000

    new updates on server configs

--- lumibot/traders/server.py
+++ lumibot/traders/server.py
@@ -9,7 +9,6 @@ class LumibotServer:
     bot_server_api_key = ""
     bot_server_api_key_name = ""
 
-    app = FastAPI()
     def __init__(self, trader, server_config):
         self.trader = trader
         self.server_config = server_config
@@ -27,71 +26,60 @@ class LumibotServer:
             self.bot_server_api_key_name = server_config.BOT_SERVER_API_KEY_NAME
         else:
             raise ValueError("BOT_SERVER_API_KEY not found in config")
-   
-    
-    api_key_header = APIKeyHeader(name=bot_server_api_key_name, auto_error=False)
 
-    # async def get_api_key(self, api_key_header: str = Security(api_key_header),
-    # ):
-    #     if api_key_header == self.bot_server_api_key:
-    #         return api_key_header
-    #     else:
-    #         raise HTTPException(
-    #             status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
-    #         )
-    
-    @app.get("/")
-    def read_root():
-        return {"message": "This is a Trading Bot Server!"}
+    async def get_api_key(self, api_key: str = Security(APIKeyHeader(name="X-API-Key", auto_error=False))):
+        if api_key == self.bot_server_api_key:
+            return api_key
+        else:
+            raise HTTPException(
+                status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+            )
 
     def start_server(self):
-        self.trader.run_all()
-        uvicorn.run(self.app, host="0.0.0.0", port=8000)
+        app = FastAPI()
+        self.bots = self.trader.run_all_async()
+
+        origins = [
+            f"http://localhost:80",
+        ]
+
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+
+        @app.get("/")
+        def read_root():
+            return {"message": "This is a Trading Bot Server!"}
+
+        @app.get("/return_history")
+        def return_history(api_key: str = Depends(self.get_api_key)):
+            strategy = self.bots["executors"][0]
+            historical_account_values = strategy.get_historical_bot_stats()
+            historical_account_values["datetime"] = historical_account_values.index
+            historical_data_dict = historical_account_values.to_dict(orient="records")
+
+            return historical_data_dict
+
+
+        @app.get("/bot_stats")
+        def bot_stats(api_key: str = Depends(self.get_api_key)):
+            # TODO: implement bot_stats
+            return {"message": "This is Bot Stat!"}
+
+
+        @app.post("/parameters")
+        def update_parameters(parameters: dict, api_key: str = Depends(self.get_api_key)):
+            strategy = self.bots["executors"][0]
+
+            strategy.update_parameters(parameters)
+
+            return {"status": "success", "parameters": strategy.parameters}
         
-        # origins = [
-        #     f"http://localhost:80",
-        # ]
-
-
-        # app.add_middleware(
-        #     CORSMiddleware,
-        #     allow_origins=origins,
-        #     allow_credentials=True,
-        #     allow_methods=["*"],
-        #     allow_headers=["*"],
-        # )
-
-        # self.api_key_header = APIKeyHeader(name=self.bot_server_api_key_name, auto_error=False)
-
-        # @app.get("/")
-        # def read_root():
-        #     return {"message": "This is a Trading Bot Server!"}
-
-        # @app.get("/return_history")
-        # def return_history(api_key: APIKey = Depends(self.bot_server_api_key)):
-        #     strategy = self.bot["executors"][0]
-
-        #     historical_account_values = strategy.get_historical_bot_stats()
-        #     historical_account_values["datetime"] = historical_account_values.index
-        #     historical_data_dict = historical_account_values.to_dict(orient="records")
-
-        #     return historical_data_dict
-
-
-        # @app.get("/bot_stats")
-        # def bot_stats(api_key: APIKey = Depends(self.bot_server_api_key)):
-        #     return get_bot_stats(self.bot)
-
-
-        # @app.post("/parameters")
-        # def update_parameters(parameters: dict, api_key: APIKey = Depends(self.bot_server_api_key)):
-        #     strategy = self.bot["executors"][0]
-
-        #     strategy.update_parameters(parameters)
-
-        #     # TODO: Add error handling
-
-        #     return {"status": "success", "parameters": strategy.parameters}
         
-    
+        uvicorn.run(app, host="127.0.0.1", port=8080)
         

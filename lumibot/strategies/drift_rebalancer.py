de5774a4ed1e3184ecf30e230813c3326ce0857f
Brett Elliot <brett@theelliots.net>
2024-11-02 14:43:49 -0400

    fix comments and order of file

--- lumibot/strategies/drift_rebalancer.py
+++ lumibot/strategies/drift_rebalancer.py
@@ -5,7 +5,6 @@ import time
 import logging
 
 from lumibot.strategies.strategy import Strategy
-from lumibot.tools import prettify_dataframe_with_decimals
 
 logger = logging.getLogger(__name__)
 # print_full_pandas_dataframes()
@@ -25,6 +24,137 @@ Execute Orders: Place limit orders to buy or sell assets based on the calculated
 Note: If you run this strategy in a live trading environment, be sure to not make manual trades in the same account.
 This strategy will sell other positions in order to get the account to the target weights. 
 """
+
+
+class DriftRebalancer(Strategy):
+    """The DriftRebalancer strategy rebalances a portfolio based on drift from target weights.
+
+    The strategy calculates the drift of each asset in the portfolio and triggers a rebalance if the drift exceeds
+    the absolute_drift_threshold. The strategy will sell assets that have drifted above the threshold and
+    buy assets that have drifted below the threshold.
+
+    The current version of the DriftRebalancer strategy only supports limit orders and whole share quantities.
+    Submit an issue if you need market orders or fractional shares. It should be pretty easy to add.
+
+    Example parameters:
+
+    parameters = {
+
+        ### Standard lumibot strategy parameters
+        "market": "NYSE",
+        "sleeptime": "1D",
+
+        ### DriftRebalancer parameters
+
+        # This is the absolute drift threshold that will trigger a rebalance. If the target_weight is 0.30 and the
+        # absolute_drift_threshold is 0.05, then the rebalance will be triggered when the assets current_weight
+        # is less than 0.25 or greater than 0.35.
+        "absolute_drift_threshold": "0.05",
+
+        # This is the acceptable slippage that will be used when calculating the number of shares to buy or sell.
+        # The default is 0.0005 (5 BPS)
+        "acceptable_slippage": "0.0005",
+
+         # The amount of time to sleep between the sells and buys to give enough time for the orders to fill
+        "fill_sleeptime": 15,
+
+        # The target weights for each asset in the portfolio. You can put the quote asset in here (or not).
+        "target_weights": {
+            "SPY": "0.60",
+            "TLT": "0.40",
+            "USD": "0.00",
+        }
+    }
+    """
+
+    # noinspection PyAttributeOutsideInit
+    def initialize(self, parameters: Any = None) -> None:
+        self.set_market(self.parameters.get("market", "NYSE"))
+        self.sleeptime = self.parameters.get("sleeptime", "1D")
+        self.absolute_drift_threshold = Decimal(self.parameters.get("absolute_drift_threshold", "0.20"))
+        self.acceptable_slippage = Decimal(self.parameters.get("acceptable_slippage", "0.0005"))
+        self.fill_sleeptime = self.parameters.get("fill_sleeptime", 15)
+        self.target_weights = {k: Decimal(v) for k, v in self.parameters["target_weights"].items()}
+        self.drift_df = pd.DataFrame()
+
+        # Sanity checks
+        if self.acceptable_slippage >= self.absolute_drift_threshold:
+            raise ValueError("acceptable_slippage must be less than absolute_drift_threshold")
+        if self.absolute_drift_threshold >= Decimal("1.0"):
+            raise ValueError("absolute_drift_threshold must be less than 1.0")
+        for key, target_weight in self.target_weights.items():
+            if self.absolute_drift_threshold >= target_weight:
+                logger.warning(
+                    f"absolute_drift_threshold of {self.absolute_drift_threshold} is "
+                    f">= target_weight of {key}: {target_weight}. Drift in this asset will never trigger a rebalance."
+                )
+
+    # noinspection PyAttributeOutsideInit
+    def on_trading_iteration(self) -> None:
+        dt = self.get_datetime()
+        logger.info(f"{dt} on_trading_iteration called")
+        self.cancel_open_orders()
+
+        if self.cash < 0:
+            logger.error(f"Negative cash: {self.cash} but DriftRebalancer does not support short sales or margin yet.")
+
+        drift_calculator = DriftCalculationLogic(target_weights=self.target_weights)
+
+        # Get all positions and add them to the calculator
+        positions = self.get_positions()
+        for position in positions:
+            symbol = position.symbol
+            current_quantity = Decimal(position.quantity)
+            if position.asset == self.quote_asset:
+                is_quote_asset = True
+                current_value = Decimal(position.quantity)
+            else:
+                is_quote_asset = False
+                current_value = Decimal(self.get_last_price(symbol)) * current_quantity
+            drift_calculator.add_position(
+                symbol=symbol,
+                is_quote_asset=is_quote_asset,
+                current_quantity=current_quantity,
+                current_value=current_value
+            )
+
+        self.drift_df = drift_calculator.calculate()
+
+        # Check if the absolute value of any drift is greater than the threshold
+        rebalance_needed = False
+        for index, row in self.drift_df.iterrows():
+            if row["absolute_drift"] > self.absolute_drift_threshold:
+                rebalance_needed = True
+                msg = (
+                    f"Absolute drift for {row['symbol']} is {row['absolute_drift']:.2f} "
+                    f"and exceeds threshold of {self.absolute_drift_threshold:.2f}"
+                )
+                logger.info(msg)
+                self.log_message(msg, broadcast=True)
+
+        if rebalance_needed:
+            msg = f"Rebalancing portfolio."
+            logger.info(msg)
+            self.log_message(msg, broadcast=True)
+            rebalance_logic = LimitOrderRebalanceLogic(
+                strategy=self,
+                df=self.drift_df,
+                fill_sleeptime=self.fill_sleeptime,
+                acceptable_slippage=self.acceptable_slippage
+            )
+            rebalance_logic.rebalance()
+
+    def on_abrupt_closing(self):
+        dt = self.get_datetime()
+        logger.info(f"{dt} on_abrupt_closing called")
+        self.log_message("On abrupt closing called.", broadcast=True)
+        self.cancel_open_orders()
+
+    def on_bot_crash(self, error):
+        dt = self.get_datetime()
+        logger.info(f"{dt} on_bot_crash called")
+        self.log_message(f"Bot crashed with error: {error}", broadcast=True)
+        self.cancel_open_orders()
 
 
 class DriftCalculationLogic:
@@ -157,133 +287,3 @@ class LimitOrderRebalanceLogic:
             limit_price=float(limit_price)
         )
         self.strategy.submit_order(limit_order)
-
-
-class DriftRebalancer(Strategy):
-    """The DriftRebalancer strategy rebalances a portfolio based on drift from target weights.
-
-    The strategy calculates the drift of each asset in the portfolio and triggers a rebalance if the drift exceeds
-    the absolute_drift_threshold. The strategy will sell assets that have drifted above the threshold and
-    buy assets that have drifted below the threshold.
-
-    The current version of the DriftRebalancer strategy only supports limit orders and whole share quantities.
-    Submit an issue if you need market orders or fractional shares. It should be pretty easy to add.
-
-    Example parameters:
-
-    parameters = {
-
-        ### Standard lumibot strategy parameters
-        "market": "NYSE",
-        "sleeptime": "1D",
-
-        ### DriftRebalancer parameters
-
-        # This is the absolute drift threshold that will trigger a rebalance. If the target_weight is 0.30 and the
-        # absolute_drift_threshold is 0.05, then the rebalance will be triggered when the assets current_weight
-        # is less than 0.25 or greater than 0.35.
-        "absolute_drift_threshold": "0.05",
-
-        # This is the acceptable slippage that will be used when calculating the number of shares to buy or sell.
-        # The default is 0.0005 (5 BPS)
-        "acceptable_slippage": "0.0005",
-
-         # The amount of time to sleep between the sells and buys to give enough time for the orders to fill
-        "fill_sleeptime": 15,
-
-        # The target weights for each asset in the portfolio. You can put the quote asset in here (or not).
-        "target_weights": {
-            "SPY": "0.60",
-            "TLT": "0.40",
-            "USD": "0.00",
-        }
-    }
-    """
-
-    # noinspection PyAttributeOutsideInit
-    def initialize(self, parameters: Any = None) -> None:
-        self.set_market(self.parameters.get("market", "NYSE"))
-        self.sleeptime = self.parameters.get("sleeptime", "1D")
-        self.absolute_drift_threshold = Decimal(self.parameters.get("absolute_drift_threshold", "0.20"))
-        self.acceptable_slippage = Decimal(self.parameters.get("acceptable_slippage", "0.0005"))
-        self.fill_sleeptime = self.parameters.get("fill_sleeptime", 15)
-        self.target_weights = {k: Decimal(v) for k, v in self.parameters["target_weights"].items()}
-        self.drift_df = pd.DataFrame()
-
-        # Sanity checks
-        if self.acceptable_slippage >= self.absolute_drift_threshold:
-            raise ValueError("acceptable_slippage must be less than absolute_drift_threshold")
-        if self.absolute_drift_threshold >= Decimal("1.0"):
-            raise ValueError("absolute_drift_threshold must be less than 1.0")
-        for key, target_weight in self.target_weights.items():
-            if self.absolute_drift_threshold >= target_weight:
-                logger.warning(
-                    f"absolute_drift_threshold of {self.absolute_drift_threshold} is "
-                    f">= target_weight of {key}: {target_weight}. Drift in this asset will never trigger a rebalance."
-                )
-
-    def on_trading_iteration(self) -> None:
-        dt = self.get_datetime()
-        logger.info(f"{dt} on_trading_iteration called")
-        self.cancel_open_orders()
-
-        if self.cash < 0:
-            logger.error(f"Negative cash: {self.cash} but DriftRebalancer does not currently support margin trading.")
-
-        drift_calculator = DriftCalculationLogic(target_weights=self.target_weights)
-
-        # Get all positions and add them to the calculator
-        positions = self.get_positions()
-        for position in positions:
-            symbol = position.symbol
-            current_quantity = Decimal(position.quantity)
-            if position.asset == self.quote_asset:
-                is_quote_asset = True
-                current_value = Decimal(position.quantity)
-            else:
-                is_quote_asset = False
-                current_value = Decimal(self.get_last_price(symbol)) * current_quantity
-            drift_calculator.add_position(
-                symbol=symbol,
-                is_quote_asset=is_quote_asset,
-                current_quantity=current_quantity,
-                current_value=current_value
-            )
-
-        self.drift_df = drift_calculator.calculate()
-
-        # Check if the absolute value of any drift is greater than the threshold
-        rebalance_needed = False
-        for index, row in self.drift_df.iterrows():
-            if row["absolute_drift"] > self.absolute_drift_threshold:
-                rebalance_needed = True
-                msg = (
-                    f"Absolute drift for {row['symbol']} is {row['absolute_drift']:.2f} "
-                    f"and exceeds threshold of {self.absolute_drift_threshold:.2f}"
-                )
-                logger.info(msg)
-                self.log_message(msg, broadcast=True)
-
-        if rebalance_needed:
-            msg = f"Rebalancing portfolio."
-            logger.info(msg)
-            self.log_message(msg, broadcast=True)
-            rebalance_logic = LimitOrderRebalanceLogic(
-                strategy=self,
-                df=self.drift_df,
-                fill_sleeptime=self.fill_sleeptime,
-                acceptable_slippage=self.acceptable_slippage
-            )
-            rebalance_logic.rebalance()
-
-    def on_abrupt_closing(self):
-        dt = self.get_datetime()
-        logger.info(f"{dt} on_abrupt_closing called")
-        self.log_message("On abrupt closing called.", broadcast=True)
-        self.cancel_open_orders()
-
-    def on_bot_crash(self, error):
-        dt = self.get_datetime()
-        logger.info(f"{dt} on_bot_crash called")
-        self.log_message(f"Bot crashed with error: {error}", broadcast=True)
-        self.cancel_open_orders()

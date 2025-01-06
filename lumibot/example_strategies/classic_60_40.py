5bbfc6774d3303437bb6b7d6bdab304b06c19701
Brett Elliot <brett@theelliots.net>
2025-01-05 19:45:03 -0500

    Added ConfigsHelper to components

--- lumibot/example_strategies/classic_60_40.py
+++ lumibot/example_strategies/classic_60_40.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-from lumibot.components.drift_rebalancer_logic import DriftType
-from lumibot.entities import Order
+from lumibot.components.configs_helper import ConfigsHelper
 from lumibot.credentials import IS_BACKTESTING
 from lumibot.example_strategies.drift_rebalancer import DriftRebalancer
+from lumibot.backtesting import YahooDataBacktesting
 
 """
 Strategy Description
@@ -17,49 +17,28 @@ assets that has drifted the least to bring the portfolio back to the target weig
 
 if __name__ == "__main__":
 
+    configs_helper = ConfigsHelper()
+    parameters = configs_helper.load_config("classic_60_40")
+
     if not IS_BACKTESTING:
         print("This strategy is not meant to be run live. Please set IS_BACKTESTING to True.")
         exit()
-    else:
 
-        parameters = {
-            "market": "NYSE",
-            "sleeptime": "1D",
+    backtesting_start = datetime(2023, 1, 2)
+    backtesting_end = datetime(2025, 1, 1)
 
-            # Pro tip: In live trading rebalance multiple times a day, more buys will be placed after the sells fill.
-            # This will make it really likely that you will complete the rebalance in a single day.
-            # "sleeptime": 60,
+    results = DriftRebalancer.backtest(
+        YahooDataBacktesting,
+        backtesting_start,
+        backtesting_end,
+        benchmark_asset="SPY",
+        parameters=parameters,
+        show_plot=True,
+        show_tearsheet=False,
+        save_tearsheet=False,
+        show_indicators=False,
+        save_logfile=True,
+        show_progress_bar=True
+    )
 
-            "drift_type": DriftType.RELATIVE,
-            "drift_threshold": "0.1",
-            "order_type": Order.OrderType.MARKET,
-            "acceptable_slippage": "0.005",  # 50 BPS
-            "fill_sleeptime": 15,
-            "target_weights": {
-                "SPY": "0.60",
-                "TLT": "0.40"
-            },
-            "shorting": False
-        }
-
-        from lumibot.backtesting import YahooDataBacktesting
-
-        backtesting_start = datetime(2023, 1, 2)
-        backtesting_end = datetime(2024, 10, 31)
-
-        results = DriftRebalancer.backtest(
-            YahooDataBacktesting,
-            backtesting_start,
-            backtesting_end,
-            benchmark_asset="SPY",
-            parameters=parameters,
-            show_plot=True,
-            show_tearsheet=False,
-            save_tearsheet=False,
-            show_indicators=False,
-            save_logfile=True,
-            # show_progress_bar=False,
-            # quiet_logs=False
-        )
-
-        print(results)
+    print(results)

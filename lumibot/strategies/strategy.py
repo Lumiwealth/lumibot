9aea526270426a0706e65f930a5859e8a72aedf9
Brett Elliot <brett@theelliots.net>
2025-02-15 10:29:31 -0500

    Revert "Merge branch 'dev' into be-alpaca-backtesting"
    This reverts commit bbbfabeb1773b1ec547ddf335924791bd7300e85, reversing
    changes made to 5efc8544e34025c2607dcbe49c40242c214bf588.

--- lumibot/strategies/strategy.py
+++ lumibot/strategies/strategy.py
@@ -115,11 +115,6 @@ class Strategy(_Strategy):
 
         The lifecycle method on_trading_iteration is executed inside a loop that stops only when there is only minutes_before_closing minutes remaining before market closes. By default equals to 5 minutes.
 
-        Parameters
-        ----------
-        minutes_before_closing : int
-            The number of minutes before market closes that the strategy will stop executing.
-
         Returns
         -------
         minutes_before_closing : int
@@ -3869,7 +3864,6 @@ class Strategy(_Strategy):
         show_progress_bar: bool = True,
         quiet_logs: bool = True,
         trader_class: Type[Trader] = Trader,
-        include_cash_positions=False,
         **kwargs,
     ):
         """Backtest a strategy.
@@ -4031,7 +4025,6 @@ class Strategy(_Strategy):
             show_progress_bar=show_progress_bar,
             quiet_logs=quiet_logs,
             trader_class=trader_class,
-            include_cash_positions=include_cash_positions,
             **kwargs,
         )
         return results

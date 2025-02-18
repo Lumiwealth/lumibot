cddbdceb77a3cbcf1077e0facb8af5d391c8f1cc
Brett Elliot <brett@theelliots.net>
2025-02-17 21:28:15 -0500

    hour timestep support in AlpacaBacktesting

--- lumibot/example_strategies/crypto_50_50.py
+++ lumibot/example_strategies/crypto_50_50.py
@@ -1,80 +1,72 @@
 from datetime import datetime
-import logging
-from decimal import Decimal
 
-from lumibot.credentials import broker
-from lumibot.backtesting import YahooDataBacktesting
-from lumibot.brokers import Alpaca
+from lumibot.components.configs_helper import ConfigsHelper
+from lumibot.credentials import IS_BACKTESTING
 from lumibot.example_strategies.drift_rebalancer import DriftRebalancer
-from lumibot.components.drift_rebalancer_logic import DriftType
-from lumibot.entities import Order, Asset
-from lumibot.traders.debug_log_trader import DebugLogTrader
+from lumibot.backtesting import AlpacaBacktesting, PandasDataBacktesting
+from lumibot.credentials import ALPACA_CONFIG
 
-logger = logging.getLogger(__name__)
+"""
+Strategy Description
+
+This strategy demonstration shows the DriftRebalancer hourly data using AlpacaBacktesting. 
+It rebalances a portfolio of assets to a target weight every time the asset drifts
+by a certain threshold. The strategy will sell the assets that has drifted the most and buy the
+assets that has drifted the least to bring the portfolio back to the target weights.
+"""
+
 
 if __name__ == "__main__":
-    is_live = False
 
-    parameters = {
-        "market": "24/7",
-        "sleeptime": "1D",
-        "drift_type": DriftType.ABSOLUTE,
-        "drift_threshold": "0.05",  # 5%
-        "order_type": Order.OrderType.LIMIT,
-        "acceptable_slippage": "0.005",  # 50 BPS
-        "fill_sleeptime": 15,
-        "shorting": False,
-        "fractional_shares": True,
-        "only_rebalance_drifted_assets": False,
+    configs_helper = ConfigsHelper(configs_folder="example_strategies")
+    parameters = configs_helper.load_config("crypto_50_50_config")
 
-    }
+    if not IS_BACKTESTING:
+        print("This strategy is not meant to be run live. Please set IS_BACKTESTING to True.")
+        exit()
 
-    if not is_live:
-        # Backtest this strategy
-        backtesting_start = datetime(2023, 1, 1)
-        backtesting_end = datetime(2023, 8, 1)
+    if not ALPACA_CONFIG:
+        print("This strategy requires an ALPACA_CONFIG config file to be set.")
+        exit()
 
-        # Backtesting crypto using yahoo means we need to use stock assets.
-        parameters["portfolio_weights"] = [
-            {
-                "base_asset": Asset(symbol='BTC-USD', asset_type='stock'),
-                "weight": Decimal("0.5")
-            },
-            {
-                "base_asset": Asset(symbol='ETH-USD', asset_type='stock'),
-                "weight": Decimal("0.5")
-            }
-        ]
-        results = DriftRebalancer.backtest(
-            YahooDataBacktesting,
-            backtesting_start,
-            backtesting_end,
-            benchmark_asset="SPY",
-            parameters=parameters,
-            show_plot=True,
-            show_tearsheet=True,
-            save_tearsheet=False,
-            show_indicators=False,
-            save_logfile=False,
-            show_progress_bar=True,
-            include_cash_positions=True,
-            trader_class=DebugLogTrader
+    if not ALPACA_CONFIG['PAPER']:
+        print(
+            "Even though this is a backtest, and only uses the alpaca keys for the data source"
+            "you should use paper keys."
         )
+        exit()
 
-    elif isinstance(broker, Alpaca):
-        # Run the strategy live
-        logger.info("Running the strategy live with alpaca.")
+    backtesting_start = datetime(2025, 1, 1)
+    backtesting_end = datetime(2025, 2, 1)
+    tickers = ["BTC/USD", "ETH/USD"]  # must be the Alpaca tickers for the Assets in the config file
+    timestep = 'hour'
 
-        # Trading crypto live means we need to use crypto assets.
-        parameters["portfolio_weights"] = [
-            {
-                "base_asset": Asset(symbol='BTC', asset_type='crypto'),
-                "weight": Decimal("0.5")
-            },
-            {
-                "base_asset": Asset(symbol='ETH', asset_type='crypto'),
-                "weight": Decimal("0.5")
-            }
-        ]
-        strategy = DriftRebalancer(broker, parameters=parameters)
-        strategy.run_live()
+    data_source = AlpacaBacktesting(
+        tickers=tickers,
+        start_date=backtesting_start.date().isoformat(),
+        end_date=backtesting_end.date().isoformat(),
+        timestep=timestep,
+        config=ALPACA_CONFIG,
+    )
+
+    strategy: DriftRebalancer
+    results, strategy = DriftRebalancer.run_backtest(
+        datasource_class=PandasDataBacktesting,
+        pandas_data=data_source.pandas_data,
+        backtesting_start=backtesting_start,
+        backtesting_end=backtesting_end,
+        parameters=parameters,
+        show_plot=False,
+        show_tearsheet=False,
+        save_tearsheet=False,
+        show_indicators=False,
+        save_logfile=False,
+        show_progress_bar=True,
+    )
+
+    trades_df = strategy.broker._trade_event_log_df # noqa
+    filled_orders = trades_df[(trades_df["status"] == "fill")]
+    print(
+        f"\nfilled_orders:\n"
+        f"{filled_orders[['time','symbol','type','side','status', 'price', 'filled_quantity', 'trade_cost']]}"
+    )

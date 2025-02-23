f1e4e76b672f53a758408fdc7bcfe9f163da9463
Brett Elliot <brett@theelliots.net>
2025-02-23 15:20:51 -0500

    enable crypto assets for PandasData

--- tests/test_pandas_data.py
+++ tests/test_pandas_data.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 
 from lumibot.data_sources import PandasData
+from lumibot.entities import Asset
+
 from tests.fixtures import (
     pandas_data_fixture,
     pandas_data_fixture_amzn_day,
-    pandas_data_fixture_amzn_minute
+    pandas_data_fixture_amzn_minute,
+    pandas_data_fixture_btc_hourly
 )
 
 
@@ -56,6 +59,41 @@ class TestPandasData:
         assert data.timestep == 'minute'
         assert data.df.index[0].isoformat() == '2025-01-13T04:00:00-05:00'
         assert data.df.index[-1].isoformat() == '2025-01-17T19:59:00-05:00'
+
+    def test_pandas_data_fixture_btc_hourly_in_pandas_data(self, pandas_data_fixture_btc_hourly):
+        backtesting_start = datetime(2021, 3, 26)
+        backtesting_end = datetime(2021, 4, 25)
+        data_source = PandasData(
+            datetime_start=backtesting_start,
+            datetime_end=backtesting_end,
+            pandas_data=pandas_data_fixture_btc_hourly
+        )
+
+        data = pandas_data_fixture_btc_hourly[0]
+        bars = data_source.get_bars(
+            assets=data.asset,
+            length=5,
+            timestep="minute",
+            quote=Asset("USD", asset_type='forex')
+        )
+        assert len(bars) == 1
+        assert bars[data.asset] is not None
+        assert bars[data.asset].asset.symbol == 'BTC'
+        assert bars[data.asset].df is not None
+        assert len(bars[data.asset].df) == 5
+
+        # Test getting the same data with a different key
+        asset_tuple = (Asset("BTC", asset_type='crypto'), Asset("USD", asset_type='forex'))
+        bars = data_source.get_bars(
+            assets=asset_tuple,
+            length=5,
+            timestep="minute",
+        )
+        assert len(bars) == 1
+        assert bars[asset_tuple] is not None
+        assert bars[asset_tuple].asset.symbol == 'BTC'
+        assert bars[asset_tuple].df is not None
+        assert len(bars[asset_tuple].df) == 5
 
 
 

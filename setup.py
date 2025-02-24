9b7fc6ad8a57962287f2d8a509d828ec05acd1bd
Joel Brass <joel@jbrass.com>
2025-02-24 02:16:18 -0800

    Update setup.py
    The Asset class recently started using the StrEnum import. This appears to be python 3.11 feature.

--- setup.py
+++ setup.py
@@ -55,5 +55,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.11",
 )

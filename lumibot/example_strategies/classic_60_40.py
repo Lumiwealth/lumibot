c596cdb66065b76a7f7b86ad687749588aa30fee
Brett Elliot <brett@theelliots.net>
2025-03-19 07:04:44 -0400

    update example strategies; fix df copy warnings and fill warnings;

--- lumibot/example_strategies/classic_60_40.py
+++ lumibot/example_strategies/classic_60_40.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+import pytz
 
 from lumibot.components.configs_helper import ConfigsHelper
 from lumibot.credentials import IS_BACKTESTING, ALPACA_TEST_CONFIG
 from lumibot.example_strategies.drift_rebalancer import DriftRebalancer
 from lumibot.backtesting import AlpacaBacktesting
 from lumibot.tools.pandas import print_full_pandas_dataframes
+from lumibot.traders.debug_log_trader import DebugLogTrader
 
 print_full_pandas_dataframes()
 
@@ -28,18 +30,45 @@ if __name__ == "__main__":
         print("This strategy is not meant to be run live. Please set IS_BACKTESTING to True.")
         exit()
 
+    if not ALPACA_TEST_CONFIG:
+        print("This strategy requires an ALPACA_TEST_CONFIG config file to be set.")
+        exit()
+
+    if not ALPACA_TEST_CONFIG['PAPER']:
+        print(
+            "Even though this is a backtest, and only uses the alpaca keys for the data source"
+            "you should use paper keys."
+        )
+        exit()
+
+    tzinfo = pytz.timezone('America/New_York')
+    backtesting_start = tzinfo.localize(datetime(2023, 1, 12))
+    backtesting_end = tzinfo.localize(datetime(2025, 1, 1))
+    timestep = 'day'
+    auto_adjust = True
+    warm_up_trading_days = 0
+    refresh_cache = False
+
     results, strategy = DriftRebalancer.run_backtest(
         datasource_class=AlpacaBacktesting,
-        backtesting_start= datetime(2023, 1, 2),
-        backtesting_end=datetime(2025, 1, 1),
+        backtesting_start=backtesting_start,
+        backtesting_end=backtesting_end,
+        minutes_before_closing=0,
+        benchmark_asset=None,
         analyze_backtest=False,
         parameters=parameters,
-        show_progress_bar=True,
 
         # AlpacaBacktesting kwargs
-        tickers=['SPY', 'TLT'],
-        timestep='day',
+        timestep=timestep,
+        market=parameters['market'],
         config=ALPACA_TEST_CONFIG,
+        refresh_cache=refresh_cache,
+        warm_up_trading_days=warm_up_trading_days,
+        auto_adjust=auto_adjust,
+
+        # For seeing logs (if using DebugLogTrader, set show_progress_bar to false)
+        # trader_class=DebugLogTrader,
+        # show_progress_bar=False,
     )
 
     print(results)

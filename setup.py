2c79154a8d15cf4fde8b6bf078e9e131a460a5c7
Robert Grzesik <rob.grzesik@gmail.com>
2025-07-04 14:10:56 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lumibot",
-    version="3.17.2",
+    version="3.17.3",
     author="Robert Grzesik",
     author_email="rob@lumiwealth.com",
     description="Backtesting and Trading Library, Made by Lumiwealth",
